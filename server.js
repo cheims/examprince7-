3b549372b382404c0497a3f943e2569263adac11
Jaime Ruiz <jaimeruizjimenez@gmail.com>
2026-06-07 19:48:56 +0200

    Initial commit: ExamPrince7Generator app

---
+++ server.js
@@ -0,0 +1,17 @@
+﻿const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const PORT = 5151;
+const ROOT = process.argv[2] || __dirname;
+
+const mime = { '.html':'text/html', '.css':'text/css', '.js':'application/javascript', '.json':'application/json', '.png':'image/png', '.ico':'image/x-icon' };
+
+http.createServer((req, res) => {
+  let filePath = path.join(ROOT, req.url === '/' ? 'index.html' : req.url);
+  fs.readFile(filePath, (err, data) => {
+    if (err) { res.writeHead(404); res.end('Not found'); return; }
+    const ext = path.extname(filePath);
+    res.writeHead(200, { 'Content-Type': mime[ext] || 'text/plain' });
+    res.end(data);
+  });
+}).listen(PORT, () => console.log('Servidor escuchando en http://localhost:' + PORT));
